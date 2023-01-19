a145a69438d8e874138be5821731ba742dca2366
Welliton Fernandes Leal <wellfernandes@me.com>
2023-01-18 22:28:51 -0300

    added client

---
+++ src/Client.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static Socket socket;
+    private static String ip;
+    private static int serverPort;
+
+    private static PrintStream out;
+    private static Scanner scanner;
+
+    public static void main(String[] args) throws IOException {
+
+        ip = "localhost";
+        serverPort = 5500;
+        try{
+            socket = new Socket(ip, serverPort);
+            System.out.println("connected client.");
+
+           scanner = new Scanner(System.in);
+            out = new PrintStream(socket.getOutputStream());
+            out.println(scanner.nextLine());
+        }catch(IOException e){
+            System.out.println("error connecting to server." +
+                    "->: "+ e.getStackTrace());
+        }finally {
+            out.close();
+            scanner.close();
+            socket.close();
+        }
+
+
+
+        while(scanner.hasNext()){
+            out.println("Texto: "+ scanner.nextLine());
+        }
+    }
+}
