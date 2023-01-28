22a9021b190970d845c9a87d48203c7b0c993894
Welliton Fernandes Leal <wellfernandes@me.com>
2023-01-28 14:21:51 -0300

    implemented multi-threaded connection

--- src/Server.java
+++ src/Server.java
@@ -2,32 +2,41 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server extends Thread{
     private static ServerSocket serverSocket;
-    private static Socket socket;
+    private Socket socket;
     private static int serverPort;
+
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try{
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String received = dataInputStream.readUTF();
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            String serverOut = dataInputStream.readUTF();
+            socket.close();
+        }catch (Exception e){
+            System.out.println("error connecting to port "+ serverPort
+                    + "-> "+ e.getStackTrace());
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
         serverPort = 5500;
         serverSocket = new ServerSocket(serverPort);
-        socket = new Socket();
         System.out.println("server on");
 
-        try{
+        while(true){
             Socket socket = serverSocket.accept();
-
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-            String serverOut = dataInputStream.readUTF();
-            dataOutputStream.writeUTF(serverOut);
-
-        }catch (IOException e){
-            System.out.println("error connecting to port "+ serverPort
-            + "-> "+ e.getStackTrace());
-        }finally {
-            socket.close();
+            Server serverThread = new Server(socket);
+            System.out.println("client connected! "+ socket.getLocalAddress());
+            serverThread.start();
         }
     }
 }
